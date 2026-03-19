dd68bbd65137434d1d5703ec083820a778a2b0a6
Lithika Damnod <l.d.b.dasanayaka@gmail.com>
2026-03-19 23:51:40 +0530

    chore: add loading feedback for create, edit, and delete actions

--- apps/customer-portal/microapp/src/context/AppProvider.tsx
+++ apps/customer-portal/microapp/src/context/AppProvider.tsx
@@ -1,4 +1,5 @@
 import LayoutProvider from "./layout/LayoutProvider";
+import LoaderProvider from "./loader/LoaderProvider";
 import ProjectProvider from "./project/ProjectProvider";
 import { ColorModeProvider } from "./theme";
 
@@ -6,7 +7,9 @@ export default function AppProvider({ children }: { children: React.ReactNode })
   return (
     <ColorModeProvider>
       <LayoutProvider>
-        <ProjectProvider>{children}</ProjectProvider>
+        <LoaderProvider>
+          <ProjectProvider>{children}</ProjectProvider>
+        </LoaderProvider>
       </LayoutProvider>
     </ColorModeProvider>
   );
